86d0864a010aa123bf0e38cb26a8d62d173bf7ea
jmarkwford <34579600+jmarkwford@users.noreply.github.com>
2023-09-05 09:17:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.header ('FruityVice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? This normalizes the fruityvice reponse 
