87a4617e4c3faf8d0e55325ea99721b81328b3da
jmarkwford <34579600+jmarkwford@users.noreply.github.com>
2023-09-05 16:01:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,15 +32,15 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 #New Section to display Fruityvice API Response
 streamlit.header ('FruityVice Fruit Advice!')
 try: 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error ("Please select a fruit to information.")
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error ("Please select a fruit to information.")
 else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 # write your own comment -what does the next line do? This normalizes the fruityvice reponse 
 
