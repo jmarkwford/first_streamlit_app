7c3d99d1d59cef778b186c3b862e65364d850551
jmarkwford <34579600+jmarkwford@users.noreply.github.com>
2023-09-05 09:38:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', + fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? This normalizes the fruityvice reponse 
